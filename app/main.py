c98f21782b0b9fcb24764f0cf8b7d504dfcd0e9f
HorusMaster <ramswi@gmail.com>
2021-09-22 11:14:18 -0500

    caca2

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
 import pathlib
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+import io
+import uuid
+from fastapi import (
+    FastAPI,
+    Header,
+    HTTPException,
+    Request,
+    File,
+    UploadFile
+    )
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
+from PIL import Image
+import pytesseract
 
 BASE_DIR = pathlib.Path(__file__).parent
+UPLOAD_DIR = BASE_DIR / "uploads"
+
 
 app = FastAPI()
 
@@ -15,5 +28,28 @@ def home_view(request: Request):
     return templates.TemplateResponse("home.html", {"request": request, "abc": 123})
 
 @app.post("/")
-def home_detail_view():
-    return {"hello": "world"}
+async def prediction_view(file:UploadFile = File(...)):
+    UPLOAD_DIR.mkdir(exist_ok=True)
+    bytes_str = io.BytesIO(await file.read())
+    try:
+        img = Image.open(bytes_str)
+    except:
+         raise HTTPException(detail="Invalid image", status_code=400)
+    preds = pytesseract.image_to_string(img)
+    predictions = [x for x in preds.split("\n")]
+    return {"results": predictions, "original": preds}
+
+
+@app.post("/img-echo/", response_class=FileResponse)
+async def img_echo_view(file:UploadFile = File(...)):
+    UPLOAD_DIR.mkdir(exist_ok=True)
+    bytes_str = io.BytesIO(await file.read())
+    try:
+        img = Image.open(bytes_str)
+    except:
+         raise HTTPException(detail="Invalid image", status_code=400)
+    fname =  pathlib.Path(file.filename)
+    fext = fname.suffix #.jpg
+    dest = UPLOAD_DIR / f"{uuid.uuid1()}{fext}"
+    img.save(dest)
+    return dest
--- test_endpoints.py
+++ test_endpoints.py
@@ -1,7 +1,11 @@
+from os import path
 from fastapi import responses
+import shutil
+import time
+import io
 from fastapi.testclient import TestClient
-from app.main import app
-
+from app.main import UPLOAD_DIR, app, BASE_DIR
+from PIL import Image, ImageChops
 client = TestClient(app)
 
 def test_get_home():
@@ -10,8 +14,53 @@ def test_get_home():
     assert response.status_code == 200
     assert "text/html" in response.headers['content-type']
 
-def test_post_home():
+def test_invalid_file_upload_error():
     response = client.post("/")
-    assert response.status_code == 200
+    assert response.status_code == 422
     assert "application/json" in response.headers['content-type']
-    assert response.json() == {"hello": "world"}
+
+def test_echo_upload():
+    img_saved_path = BASE_DIR / "images"
+    for path in img_saved_path.glob("*.png"):
+        try:
+            img = Image.open(path)
+        except:
+            img = None
+        response = client.post("/img-echo/", files = {"file": open(path, 'rb')})
+        if img is None:
+            assert response.status_code == 400
+        else:
+            assert response.status_code == 200
+            r_stream = io.BytesIO(response.content)
+            echo_img = Image.open(r_stream)
+            difference = ImageChops.difference(echo_img, img).getbbox()
+            assert difference is None
+        # print(response.headers)
+
+        # assert "application/json" in response.headers['content-type']
+        # assert response.json() == {"hello": "world"}
+    #time.sleep(3)
+    shutil.rmtree(UPLOAD_DIR)
+
+
+def test_prediction_upload():
+    img_saved_path = BASE_DIR / "images"
+    for path in img_saved_path.glob("*.png"):
+        try:
+            img = Image.open(path)
+        except:
+            img = None
+        response = client.post("/", files = {"file": open(path, 'rb')})
+        if img is None:
+            assert response.status_code == 400
+        else:
+            assert response.status_code == 200
+            data = response.json()
+            print(data)
+            assert len(data.keys()) == 2
+        # print(response.headers)
+
+        # assert "application/json" in response.headers['content-type']
+        # assert response.json() == {"hello": "world"}
+    #time.sleep(3)
+    shutil.rmtree(UPLOAD_DIR)
